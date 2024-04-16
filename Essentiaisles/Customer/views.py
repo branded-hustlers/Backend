ee3967645c530a8d1381ce80aed503aa224081d9
davidberko36 <130989471+davidberko36@users.noreply.github.com>
2024-04-16 05:27:19 +0000

    Customer sign up and sign in views

--- Essentiaisles/Customer/views.py
+++ Essentiaisles/Customer/views.py
@@ -1,7 +1,11 @@
 from rest_framework import status
+from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import CustomerSerializer
+import jwt
+from datetime import datetime, timedelta, timezone
+from Essentiaisles.settings import SECRET_KEY
 
 class SignUpView(APIView):
     def post(self, request):
@@ -10,3 +14,25 @@ class SignUpView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # Generate JWT token
+            payload = {
+                    'user_id': user.id,
+                    'exp': datetime.now(timezone.utc) + timedelta(hours=24),
+                    'iat': datetime.now(timezone.utc)
+                }
+            token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+            return Response({'token': token}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
