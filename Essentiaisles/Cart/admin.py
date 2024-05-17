b5f3afdc1fff3f40600bfef57f0769ba0d916a70
David Berko <daberko364@gmail.com>
2024-05-17 14:30:57 +0000

    Some changes

--- Essentiaisles/Cart/admin.py
+++ Essentiaisles/Cart/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
-# from .models import Cart
-# from .models import CartItem
-# from .models import Order, OrderedItem
-# Register your models here.
-# class CartItemInline(admin.TabularInline):
-#     model = CartItem
+from .models import Cart
+from .models import CartItem
+from .models import Order, OrderedItem
+#Register your models here.
+class CartItemInline(admin.TabularInline):
+    model = CartItem
 
-# class CartAdmin(admin.ModelAdmin):
-#     inlines = [CartItemInline]
-
-
+class CartAdmin(admin.ModelAdmin):
+    inlines = [CartItemInline]
 
 
-# admin.site.register(Cart)
-# admin.site.register(CartItem)
-# admin.site.register(Order)
-# admin.site.register(OrderedItem)
+
+
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderedItem)
 
